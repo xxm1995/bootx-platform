f97dd2c520a9037f73ac866970735918b7e757c6
xxm <xxm1995@outlook.com>
2022-03-31 19:15:42 +0800

    demo 幂等demo

--- bootx-demo/src/main/java/cn/bootx/demo/controller/IdempotencyDemoController.java
+++ bootx-demo/src/main/java/cn/bootx/demo/controller/IdempotencyDemoController.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class IdempotencyDemoController {
 
-    @Idempotent(name = "idempotent",timeout = 1000*60)
+    @Idempotent(name = "idempotent",timeout = 1000)
     @Operation(summary = "幂等演示")
-    @GetMapping("/test")
+    @PostMapping("/test")
     public ResResult<String> Idempotent(){
         return Res.ok("幂等演示");
     }
