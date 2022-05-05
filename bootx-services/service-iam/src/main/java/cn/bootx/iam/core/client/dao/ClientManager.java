d6caad974310067b780b8cd289a782a88fe08b87
xxm <xxm1995@outlook.com>
2022-05-05 16:46:39 +0800

    fix 通过终端code获取终端详情被数据权限插件拦截

--- bootx-services/service-iam/src/main/java/cn/bootx/iam/core/client/dao/ClientManager.java
+++ bootx-services/service-iam/src/main/java/cn/bootx/iam/core/client/dao/ClientManager.java
@@ -1,5 +1,6 @@
 package cn.bootx.iam.core.client.dao;
 
+import cn.bootx.common.core.annotation.Permission;
 import cn.bootx.common.core.rest.param.PageParam;
 import cn.bootx.common.mybatisplus.impl.BaseManager;
 import cn.bootx.common.mybatisplus.util.MpUtil;
@@ -23,6 +24,8 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class ClientManager extends BaseManager<ClientMapper, Client> {
+
+    @Permission(dataScope = false,selectField = false)
     public Optional<Client> findByCode(String code) {
         return findByField(Client::getCode,code);
     }
