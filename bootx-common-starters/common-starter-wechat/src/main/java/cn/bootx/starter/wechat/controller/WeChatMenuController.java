edb187b8c71001fbe0a3aa87d3f011398bc1db58
xxm <xxm1995@outlook.com>
2022-08-07 17:07:01 +0800

    feat 公众号编辑器开发

--- bootx-common-starters/common-starter-wechat/src/main/java/cn/bootx/starter/wechat/controller/WeChatMenuController.java
+++ bootx-common-starters/common-starter-wechat/src/main/java/cn/bootx/starter/wechat/controller/WeChatMenuController.java
@@ -6,11 +6,9 @@ import cn.bootx.starter.wechat.core.menu.service.WeChatMenuService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import me.chanjar.weixin.common.bean.menu.WxMenu;
 import me.chanjar.weixin.mp.bean.menu.WxMpMenu;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 微信菜单管理
@@ -26,12 +24,12 @@ public class WeChatMenuController {
 
     @Operation(summary = "发布菜单")
     @PostMapping("/publish")
-    public ResResult<Void> publish(){
-        weChatMenuService.publish();
+    public ResResult<Void> publish(@RequestBody WxMenu menu){
+        weChatMenuService.publish(menu);
         return Res.ok();
     }
 
-    @Operation(summary = "获取菜单")
+    @Operation(summary = "获取微信菜单")
     @GetMapping("/getMenus")
     public ResResult<WxMpMenu> getMenus(){
         return Res.ok(weChatMenuService.getMenus());
--- bootx-common-starters/common-starter-wechat/src/main/java/cn/bootx/starter/wechat/core/menu/service/WeChatMenuService.java
+++ bootx-common-starters/common-starter-wechat/src/main/java/cn/bootx/starter/wechat/core/menu/service/WeChatMenuService.java
@@ -3,16 +3,11 @@ package cn.bootx.starter.wechat.core.menu.service;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.bean.menu.WxMenu;
-import me.chanjar.weixin.common.bean.menu.WxMenuButton;
 import me.chanjar.weixin.mp.api.WxMpMenuService;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.menu.WxMpMenu;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 
 /**   
  * 微信菜单
@@ -29,24 +24,13 @@ public class WeChatMenuService {
      * 添加菜单
      */
     @SneakyThrows
-    public void publish(){
+    public void publish(WxMenu menu){
         WxMpMenuService menuService = wxMpService.getMenuService();
-        WxMenu menu = new WxMenu();
-
-        WxMenuButton wxMenuButton = new WxMenuButton();
-        val a =  new WxMenuButton();
-        a.setName("测试菜单");
-        a.setType(WxConsts.MenuButtonType.PIC_SYSPHOTO);
-        a.setKey("cs");
-        wxMenuButton.setName("父菜单");
-        wxMenuButton.setSubButtons(Collections.singletonList(a));
-
-        menu.setButtons(Collections.singletonList(wxMenuButton));
         menuService.menuCreate(menu);
     }
 
     /**
-     * 获取菜单
+     * 获取微信菜单
      */
     @SneakyThrows
     public WxMpMenu getMenus(){
