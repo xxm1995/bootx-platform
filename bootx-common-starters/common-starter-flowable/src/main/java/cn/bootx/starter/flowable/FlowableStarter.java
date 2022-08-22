807f54c2fe4e725067fb2aefdbe402c442cc4c8f
xxm <xxm1995@outlook.com>
2022-08-22 17:00:52 +0800

    feat 添加工作流

---
+++ bootx-common-starters/common-starter-flowable/src/main/java/cn/bootx/starter/flowable/FlowableStarter.java
@@ -0,0 +1,14 @@
+package cn.bootx.starter.flowable;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**   
+ * 工作流
+ * @author xxm  
+ * @date 2022/8/22 
+ */
+@ComponentScan
+@ConfigurationPropertiesScan
+public class FlowableStarter {
+}
---
+++ bootx-common-starters/common-starter-flowable/src/main/java/cn/bootx/starter/flowable/config/FlowableConfiguration.java
@@ -0,0 +1,8 @@
+package cn.bootx.starter.flowable.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlowableConfiguration {
+
+}
