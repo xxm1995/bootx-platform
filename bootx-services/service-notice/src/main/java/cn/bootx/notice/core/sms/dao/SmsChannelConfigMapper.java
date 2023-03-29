b0a2bd14915379f08c07e8b39325ab04bfe037aa
xucun <yinxucun@gmail.com>
2023-03-29 16:02:54 +0800

    fix  修改引入注解类型

--- bootx-services/service-notice/src/main/java/cn/bootx/notice/core/sms/dao/SmsChannelConfigMapper.java
+++ bootx-services/service-notice/src/main/java/cn/bootx/notice/core/sms/dao/SmsChannelConfigMapper.java
@@ -2,12 +2,11 @@ package cn.bootx.notice.core.sms.dao;
 
 import cn.bootx.notice.core.sms.entity.SmsChannelConfig;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.mapstruct.Mapper;
+import org.apache.ibatis.annotations.Mapper;
 
-/**   
- * 
- * @author xxm  
- * @date 2023/3/28 
+/**
+ * @author xxm
+ * @date 2023/3/28
  */
 @Mapper
 public interface SmsChannelConfigMapper extends BaseMapper<SmsChannelConfig> {
