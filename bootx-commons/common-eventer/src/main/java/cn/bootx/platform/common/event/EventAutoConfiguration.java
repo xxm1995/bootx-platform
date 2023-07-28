561fe0ae6ba977f5720dd177d372d7ab1a511810
xxm1995 <xxm1995@outlook.com>
2023-07-28 17:08:46 +0800

    feat 事件机制

---
+++ bootx-commons/common-eventer/src/main/java/cn/bootx/platform/common/event/EventAutoConfiguration.java
@@ -0,0 +1,16 @@
+package cn.bootx.platform.common.event;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 简单事件通知封装
+ * @author xxm
+ * @since 2023/7/28
+ */
+@ComponentScan
+@ConfigurationPropertiesScan
+@AutoConfiguration
+public class EventAutoConfiguration {
+}
---
+++ bootx-commons/common-eventer/src/main/java/cn/bootx/platform/common/event/code/EventTyoeEnum.java
@@ -0,0 +1,9 @@
+package cn.bootx.platform.common.event.code;
+
+/**   
+ * 事件消息类型
+ * @author xxm  
+ * @since 2023/7/28 
+ */
+public enum EventTyoeEnum {
+}
