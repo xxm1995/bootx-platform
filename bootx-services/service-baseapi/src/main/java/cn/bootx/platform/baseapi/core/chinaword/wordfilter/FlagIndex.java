87971e23cdcf63902cde57eb39ae5e715cbb4d83
xxm1995 <xxm1995@outlook.com>
2023-08-08 20:26:13 +0800

    feat 敏感词功能开发, 新增几种MP的类型转换器类, 升级MP版本, 删除APP相关历史代码

---
+++ bootx-services/service-baseapi/src/main/java/cn/bootx/platform/baseapi/core/chinaword/wordfilter/FlagIndex.java
@@ -0,0 +1,48 @@
+package cn.bootx.platform.baseapi.core.chinaword.wordfilter;
+
+import java.util.List;
+
+/**
+ * 敏感词标记
+ *
+ * @author minghu.zhang
+ */
+public class FlagIndex {
+
+    /**
+     * 标记结果
+     */
+    private boolean flag;
+    /**
+     * 是否黑名单词汇
+     */
+    private boolean isWhiteWord;
+    /**
+     * 标记索引
+     */
+    private List<Integer> index;
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public List<Integer> getIndex() {
+        return index;
+    }
+
+    public void setIndex(List<Integer> index) {
+        this.index = index;
+    }
+
+    public boolean isWhiteWord() {
+        return isWhiteWord;
+    }
+
+    public void setWhiteWord(boolean whiteWord) {
+        isWhiteWord = whiteWord;
+    }
+}
