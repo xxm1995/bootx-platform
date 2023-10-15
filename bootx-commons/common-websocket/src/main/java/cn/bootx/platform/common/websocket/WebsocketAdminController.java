ee6ef14f231e7e87ad9d1340dd5651a47f70f29a
xxm1995 <xxm1995@outlook.com>
2023-10-15 18:58:16 +0800

    feat websocket会话管理

---
+++ bootx-commons/common-websocket/src/main/java/cn/bootx/platform/common/websocket/WebsocketAdminController.java
@@ -0,0 +1,20 @@
+package cn.bootx.platform.common.websocket;
+
+import cn.bootx.platform.common.websocket.service.WebSocketAdminService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author xxm
+ * @since 2023/10/15
+ */
+@Tag(name = "websocket会话管理")
+@RestController
+@RequestMapping("/websocket/admin")
+@RequiredArgsConstructor
+public class WebsocketAdminController {
+    private final WebSocketAdminService webSocketAdminService;
+}
---
+++ bootx-commons/common-websocket/src/main/java/cn/bootx/platform/common/websocket/service/WebSocketAdminService.java
@@ -0,0 +1,16 @@
+package cn.bootx.platform.common.websocket.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * websocket会话管理
+ * @author xxm
+ * @since 2023/10/15
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class WebSocketAdminService {
+}
