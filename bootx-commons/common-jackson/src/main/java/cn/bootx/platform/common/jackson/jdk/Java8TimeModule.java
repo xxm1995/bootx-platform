0f5ca8b59ba2f82b3bce08a5debbb3c69b2b33c0
xxm1995 <xxm1995@outlook.com>
2024-01-16 14:17:48 +0800

    feat 时间反序列化, 支持反序列化秒和毫秒级别的时间

--- bootx-commons/common-jackson/src/main/java/cn/bootx/platform/common/jackson/jdk/Java8TimeModule.java
+++ bootx-commons/common-jackson/src/main/java/cn/bootx/platform/common/jackson/jdk/Java8TimeModule.java
@@ -1,10 +1,10 @@
 package cn.bootx.platform.common.jackson.jdk;
 
+import cn.bootx.platform.common.jackson.deserializer.BootxLocalDateTimeDeserializer;
+import cn.bootx.platform.common.jackson.deserializer.BootxLocalTimeDeserializer;
 import cn.hutool.core.date.DatePattern;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
@@ -15,28 +15,27 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * java8 时间序列化 受MySQL限制, 无法存储毫秒值. 所以值处理到秒级别
- * 目标:
- * 写入时间支持传入秒和毫秒两种格式的时间, 存储时支持支持毫秒
- * 读取时间时携带毫秒信息
+ * java8 时间序列化 受MySQL限制, 无法存储毫秒值. 只能处理到秒级别
+ * 写入时间支持毫秒格式的时间
+ * 读取时间时可以处理到秒或毫秒
  * @author xxm
  * @since 2020/4/14 13:33
  */
 public class Java8TimeModule extends SimpleModule {
 
     public Java8TimeModule() {
+        // 序列化
         this.addSerializer(LocalDateTime.class,
                 new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
         this.addSerializer(LocalDate.class,
                 new LocalDateSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN)));
         this.addSerializer(LocalTime.class,
                 new LocalTimeSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN)));
-        this.addDeserializer(LocalDateTime.class,
-                new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
+        // 反序列化
+        this.addDeserializer(LocalDateTime.class, new BootxLocalDateTimeDeserializer());
         this.addDeserializer(LocalDate.class,
                 new LocalDateDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN)));
-        this.addDeserializer(LocalTime.class,
-                new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN)));
+        this.addDeserializer(LocalTime.class,new BootxLocalTimeDeserializer());
     }
 
 }
