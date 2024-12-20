90e37ae90a7fed3fbf14d304833d44d8eb674bde
DaxPay <xxm1995@outlook.com>
2024-12-20 11:22:08 +0800

    feat 钉钉模块开发

---
+++ bootx-platform-common/common-dingtalk/src/main/java/cn/bootx/platform/common/dingtalk/DingTalkAutoConfiguration.java
@@ -0,0 +1,18 @@
+package cn.bootx.platform.common.dingtalk;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 钉钉对接
+ *
+ * @author xxm
+ * @since 2022/4/2
+ */
+@ComponentScan
+@ConfigurationPropertiesScan
+@AutoConfiguration
+public class DingTalkAutoConfiguration {
+
+}
