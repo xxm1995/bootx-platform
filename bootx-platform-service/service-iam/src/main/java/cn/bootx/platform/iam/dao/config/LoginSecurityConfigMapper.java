6eff36ad420089a09592e4ca5612d4d3645c7b39
DaxPay <xxm1995@outlook.com>
2024-12-30 15:27:24 +0800

    ifx: 为接口添加 @Mapper 注解解决启动失败问题

--- bootx-platform-service/service-iam/src/main/java/cn/bootx/platform/iam/dao/config/LoginSecurityConfigMapper.java
+++ bootx-platform-service/service-iam/src/main/java/cn/bootx/platform/iam/dao/config/LoginSecurityConfigMapper.java
@@ -2,11 +2,13 @@ package cn.bootx.platform.iam.dao.config;
 
 import cn.bootx.platform.iam.entity.security.LoginSecurityConfig;
 import com.github.yulichang.base.MPJBaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 登录安全策略
  * @author xxm
  * @since 2024/12/12
  */
+@Mapper
 public interface LoginSecurityConfigMapper extends MPJBaseMapper<LoginSecurityConfig> {
 }
